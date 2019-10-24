4048337cfd90a8e7bf982e3be8f10034418ee809
Alice Pote <alice.writes.wrongs@gmail.com>
2019-10-24 15:14:17 -0400

    display for the vocab separately

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import { useDeckInfo } from "./api.js"
@@ -8,7 +8,11 @@ import { getKanjiLevels, getVocabLevels } from "./data"
 const VOCAB_DECK_NAME = "日本語::kanji_vocab"
 const KANJI_DECK_NAME = "日本語::kanji"
 
+const KANJI = "KANJI"
+const VOCAB = "VOCAB"
+
 function App() {
+  const [thingToShow, setThingToShow] = useState(KANJI)
   const [isKanjiFinished, kanjiByLevels] = useDeckInfo(
     KANJI_DECK_NAME,
     getKanjiLevels
@@ -20,14 +24,50 @@ function App() {
 
   return (
     <div className="App">
-      <div className="kanji-list">
-        {isKanjiFinished
+      <div>
+        <input
+          type="radio"
+          id="kanjiopt"
+          name="drone"
+          value={KANJI}
+          checked={thingToShow === KANJI}
+          onChange={e => {
+            setThingToShow(e.target.value)
+          }}
+        />
+        <label for="kanjiopt">Kanji</label>
+        <input
+          type="radio"
+          id="vocabobt"
+          name="drone"
+          value={VOCAB}
+          checked={thingToShow === VOCAB}
+          onChange={e => {
+            setThingToShow(e.target.value)
+          }}
+        />
+        <label for="vocabopt">Vocab</label>
+      </div>
+      <div className="item-list">
+        {isKanjiFinished && thingToShow === KANJI
           ? Object.keys(kanjiByLevels).map(level => (
               <div className="kanji-level">
                 <h2>{level}</h2>
                 <div className="kanjis">
                   {kanjiByLevels[level].map(kanji => (
                     <div className="kanji">{kanji}</div>
+                  ))}
+                </div>
+              </div>
+            ))
+          : null}
+        {isVocabFinished && thingToShow === VOCAB
+          ? Object.keys(vocabByLevels).map(level => (
+              <div className="vocab-level">
+                <h2>{level}</h2>
+                <div className="vocabs">
+                  {vocabByLevels[level].map(vocab => (
+                    <div className="vocab">{vocab}</div>
                   ))}
                 </div>
               </div>
