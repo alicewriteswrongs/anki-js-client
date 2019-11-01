29c7291df42d25d444dd4d9b2d80cdad8a1a279b
Alice Pote <alice.writes.wrongs@gmail.com>
2019-11-01 10:57:01 -0400

    got it working, all powered by redux-query!

--- src/selectors.js
+++ src/selectors.js
@@ -7,3 +7,43 @@ export const getNoteIds = createSelector(
     vocabNotes: notes ? notes.vocab : null
   })
 )
+
+export const getKanjiLevels = createSelector(
+  state => state.entities.kanji,
+  allKanji => {
+    if (!allKanji) {
+      return []
+    }
+
+    return Object.keys(allKanji).reduce((acc, kanji) => {
+      const entry = allKanji[ kanji ]
+      const tag = entry.tags[1]
+      if (acc[tag]) {
+        acc[tag].push(entry.fields.Kanji.value)
+      } else {
+        acc[tag] = [entry.fields.Kanji.value]
+      }
+      return acc
+    }, {})
+  }
+)
+
+export const getVocabLevels = createSelector(
+  state => state.entities.vocab,
+  allVocab => {
+    if (! allVocab) {
+      return []
+    }
+
+    return Object.keys(allVocab).reduce((acc, vocab) => {
+      const entry = allVocab[vocab]
+      const tag = entry.tags[0]
+      if (acc[tag]) {
+        acc[tag].push(entry.fields.Vocab.value)
+      } else {
+        acc[tag] = [entry.fields.Vocab.value]
+      }
+      return acc
+    }, {})
+  }
+)
