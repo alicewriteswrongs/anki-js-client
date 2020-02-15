59511e3bb0d51cce3c2cd7c975b35f4fa071298d
Alice Pote <alice.writes.wrongs@gmail.com>
2020-02-15 10:43:34 -0500

    add prettier, run it!

--- src/store.js
+++ src/store.js
@@ -28,8 +28,9 @@ const networkInterface = (url, method, networkOptions) => {
 
 const store = createStore(
   reducer,
-  compose(applyMiddleware(queryMiddleware(networkInterface, getQueries, getEntities)),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(queryMiddleware(networkInterface, getQueries, getEntities)),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
