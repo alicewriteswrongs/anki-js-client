548053bd919735becbf4340deebf3a2fb6cea806
Alice Pote <alice.writes.wrongs@gmail.com>
2020-03-09 13:49:11 -0400

    Updates

---
+++ precache-manifest.622c1db22ec86b72916e5ac32aa73f85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc5a46a0cb8b4e9f1cc5ed3449aa662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055ba3302f8948dd809b",
+    "url": "/static/css/main.3c88dcf5.chunk.css"
+  },
+  {
+    "revision": "d8813c8979b335482db7",
+    "url": "/static/js/2.4b4322ee.chunk.js"
+  },
+  {
+    "revision": "055ba3302f8948dd809b",
+    "url": "/static/js/main.0b4fc5bb.chunk.js"
+  },
+  {
+    "revision": "2466cf718e956a98da62",
+    "url": "/static/js/runtime-main.422eea2b.js"
+  }
+]);
