67d1e8e6052ab29f60469e27d6d336ec24873d16
Alice Pote <alice.writes.wrongs@gmail.com>
2021-04-13 11:48:00 -0400

    starting to work on this?

--- __init__.py
+++ __init__.py
@@ -2,12 +2,19 @@ from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
 
+from anki import hooks
+
 def initAddon():
-    cardCount = mw.col.cardCount()
-    showInfo("Card count: %d" % cardCount)
+    deckId = mw.col.decks.id("español")
+    deck = mw.col.decks.select(deckId)
+
+    ids = mw.col.findCards("deck:español")
+    showInfo("cards in español: %d" % len(ids))
 
 action = QAction("test", mw)
 
 action.triggered.connect(initAddon)
 
 mw.form.menuTools.addAction(action)
+
+
---
+++ dev_build.sh
@@ -0,0 +1,5 @@
+rm -rf ~/.local/share/Anki2/addons21/anki_review_dashboard
+
+mkdir ~/.local/share/Anki2/addons21/anki_review_dashboard
+
+cp __init__.py /home/alice/.local/share/Anki2/addons21/anki_review_dashboard/
