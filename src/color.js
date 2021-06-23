b6f61feb3ce2efc18254bb470e9dad10db75c505
Alice Pote <alice.writes.wrongs@gmail.com>
2021-06-23 11:35:35 -0400

    I want my colors to change more quickly

--- src/color.js
+++ src/color.js
@@ -4,7 +4,7 @@ export const intervalToColor = interval => {
     b = 0
 
   // arbitrarily high number
-  let percentage = interval > 500 ? 1 : interval / 500.0
+  let percentage = interval > 100 ? 1 : interval / 100.0
 
   percentage = percentage * 100
 
@@ -13,7 +13,7 @@ export const intervalToColor = interval => {
     g = Math.round(5.1 * percentage)
   } else {
     g = 255
-    r = Math.round(510 - 5.1 * percentage)
+    r = Math.round(256 - 5.1 * percentage)
   }
   let h = r * 0x10000 + g * 0x100 + b * 0x1
   return "#" + ("000000" + h.toString(16)).slice(-6)
